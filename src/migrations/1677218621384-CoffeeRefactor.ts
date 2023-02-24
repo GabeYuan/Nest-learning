951564c8f50d701fbb405ca396d3773fe0f700f0
yuangenbao <rootyuan@163.com>
2023-02-24 14:09:38 +0800

    数据迁移问题

--- src/migrations/1677218621384-CoffeeRefactor.ts
+++ src/migrations/1677218621384-CoffeeRefactor.ts
@@ -1,6 +1,6 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class CoffeeRefactor1677142720146 implements MigrationInterface {
+export class CoffeeRefactor1677218621384 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
       `ALTER TABLE "coffee" RENAME COLUMN "title" TO "name"`,
@@ -9,7 +9,7 @@ export class CoffeeRefactor1677142720146 implements MigrationInterface {
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `ALTER TABLE "coffee" RENAME COLUMN "title" TO "name"`,
+      `ALTER TABLE "coffee" RENAME COLUMN "name" TO "title"`,
     );
   }
 }
--- typeOrm.config.ts
+++ typeOrm.config.ts
@@ -1,11 +1,15 @@
 import { DataSource } from 'typeorm';
 // https://wanago.io/2022/07/25/api-nestjs-database-migrations-typeorm/
+// npx typeorm-ts-node-esm migration:run -d .\typeOrm.config.ts
+import { CoffeeRefactor1677218621384 } from './src/migrations/1677218621384-CoffeeRefactor';
 export default new DataSource({
   type: 'postgres',
-  // host: configService.get('POSTGRES_HOST'),
-  // port: configService.get('POSTGRES_PORT'),
-  // username: configService.get('POSTGRES_USER'),
-  // password: configService.get('POSTGRES_PASSWORD'),
-  // database: configService.get('POSTGRES_DB'),
-  // entities: [],
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: 'pass123',
+  database: 'postgres',
+  entities: ['dist/**/*.entity.js'],
+  // migrations: ['dist/migrations/*.js'],
+  migrations: [CoffeeRefactor1677218621384],
 });
