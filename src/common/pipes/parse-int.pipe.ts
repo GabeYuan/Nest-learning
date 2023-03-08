cef316fa5e36d0e23940be8f0394b5d4381294a2
yuangenbao <rootyuan@163.com>
2023-03-08 10:13:19 +0800

    Middleware

--- src/common/pipes/parse-int.pipe.ts
+++ src/common/pipes/parse-int.pipe.ts
@@ -9,7 +9,6 @@ import {
 export class ParseIntPipe implements PipeTransform {
   transform(value: string, metadata: ArgumentMetadata) {
     const val = parseInt(value, 10);
-    console.log(value, val, '************');
 
     if (isNaN(val)) {
       throw new BadRequestException(
